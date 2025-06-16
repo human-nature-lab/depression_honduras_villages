ca155d1dccace18db1ec1923feafbe465b6279b6
Marios Papamichalis <papamixmarios@gmail.com>
2025-06-16 23:52:54 +0300

    Update Save_all_relationships.R

--- Save_all_relationships.R
+++ Save_all_relationships.R
@@ -10,37 +10,6 @@ save(all_networks_df_1, file = "Depression/Code/Results/all_networks_df_1.rda")
 save(all_networks_df_3, file = "Depression/Code/Results/all_networks_df_3.rda")
 save(all_networks_df_4, file = "Depression/Code/Results/all_networks_df_4.rda")
 
-# All networks (health)
-save(all_networks_df_health_1, file = "Depression/Code/Results/all_networks_df_health_1.rda")
-save(all_networks_df_health_3, file = "Depression/Code/Results/all_networks_df_health_3.rda")
-save(all_networks_df_health_4, file = "Depression/Code/Results/all_networks_df_health_4.rda")
-
-# All networks (financial)
-save(all_networks_df_financial_1, file = "Depression/Code/Results/all_networks_df_financial_1.rda")
-save(all_networks_df_financial_3, file = "Depression/Code/Results/all_networks_df_financial_3.rda")
-save(all_networks_df_financial_4, file = "Depression/Code/Results/all_networks_df_financial_4.rda")
-
-
-# All networks (financial)
-save(individuals_wave1_fr, file = "Depression/Code/Results/individuals_wave1_fr.rda")
-save(individuals_wave3_fr, file = "Depression/Code/Results/individuals_wave3_fr.rda")
-save(individuals_wave4_fr, file = "Depression/Code/Results/individuals_wave4_fr.rda")
-
-
-# All networks (financial)
-save(gr1_fr, file = "Depression/Code/Results/gr1_fr.rda")
-save(gr3_fr, file = "Depression/Code/Results/gr3_fr.rda")
-save(gr4_fr, file = "Depression/Code/Results/gr4_fr.rda")
-
-# All networks (financial)
-save(gr1_adv,file = "Depression/Code/Results/gr1_adv.rda")
-save(gr3_adv, file = "Depression/Code/Results/gr3_adv.rda")
-save(gr4_adv, file = "Depression/Code/Results/gr4_adv.rda")
-
-
-
-# All networks (financial)
-
 save(y_c,file = "Depression/Code/Results/y_c.rda")
 save(z_c, file = "Depression/Code/Results/z_c.rda")
 
