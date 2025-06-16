2bc1efddc477899ed6051173450490a6956cb3e8
Marios Papamichalis <papamixmarios@gmail.com>
2025-06-16 23:52:26 +0300

    Update Open_all_relationships.R

--- Open_all_relationships.R
+++ Open_all_relationships.R
@@ -9,30 +9,6 @@ load("Depression/Code/Results/all_networks_df_1.rda")
 load("Depression/Code/Results/all_networks_df_3.rda")
 load("Depression/Code/Results/all_networks_df_4.rda")
 
-load("Depression/Code/Results/all_networks_df_health_1.rda")
-load("Depression/Code/Results/all_networks_df_health_3.rda")
-load("Depression/Code/Results/all_networks_df_health_4.rda")
-
-load("Depression/Code/Results/all_networks_df_financial_1.rda")
-load("Depression/Code/Results/all_networks_df_financial_3.rda")
-load("Depression/Code/Results/all_networks_df_financial_4.rda")
-
-
-# All networks (financial)
-load("Depression/Code/Results/individuals_wave1_fr.rda")
-load("Depression/Code/Results/individuals_wave3_fr.rda")
-load("Depression/Code/Results/individuals_wave4_fr.rda")
-
-# All networks (financial)
-load("Depression/Code/Results/gr1_fr.rda")
-load("Depression/Code/Results/gr3_fr.rda")
-load("Depression/Code/Results/gr4_fr.rda")
-
-# All networks (financial)
-load("Depression/Code/Results/gr1_adv.rda")
-load("Depression/Code/Results/gr3_adv.rda")
-load("Depression/Code/Results/gr4_adv.rda")
-
 load("Depression/Code/Results/y_c.rda")
 load("Depression/Code/Results/z_c.rda")
 
@@ -42,14 +18,7 @@ load("Depression/Code/Results/z_3_c.rda")
 load("Depression/Code/Results/y_4_c.rda")
 load("Depression/Code/Results/z_4_c.rda")
 
-
 load("Depression/Code/Results/y1_open.rda")
 load("Depression/Code/Results/y3_open.rda")
 load("Depression/Code/Results/y4_open.rda")
-
-
-# All networks (financial)
-load("Depression/Code/Results/final_results_1.rda")
-load("Depression/Code/Results/final_results_3.rda")
-load("Depression/Code/Results/final_results_4.rda")
 
